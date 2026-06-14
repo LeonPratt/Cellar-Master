32223ce4bb1ec11015b601e2e2ed8ec7ee65736b
unknown <leon.alex.pratt@gmail.com>
2026-06-14 17:30:58 +0100

    .

--- home.js
+++
@@ -1,8 +0,0 @@
-let add_remove = document.getElementById("add_remove")
-let search_wine = document.getElementById("search_wine")
-let search_pairing = document.getElementById("search_pairing")
-console.log("loaded")
-
-add_remove.addEventListener("click",function(){
-    window.location.href = "/camera"
-})
